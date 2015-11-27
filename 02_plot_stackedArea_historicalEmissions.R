14dbc4f6e46fa1530f0dd8ef8bc28a4657dd7573
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2015-11-27 17:12:40 +0100

    embed codes sent

--- 02_plot_stackedArea_historicalEmissions.R
+++ 02_plot_stackedArea_historicalEmissions.R
@@ -1,4 +1,3 @@
-
 library(dplyr)
 library(swiRcharts)
 library(swiTheme)
@@ -16,8 +15,6 @@ trad <- read.csv("output/historicalEmissionsTranslations.csv",
 # write.csv(data.frame(code = paste0("country.", gsub("( *|\\(|\\))", "", unique(data$Country))), en =  unique(data$Country)),
 #   file = "output/historicalEmissionsTranslations.csv", row.names = F)
 
-maxY <- data %>% group_by(Year) %>% summarise(tot = sum(`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`)) %>% select(tot) %>% max
-
 ############################################################################################
 ###		Settings chart
 ############################################################################################
@@ -34,7 +31,6 @@ for (i in 1:ncol(trad)) {
 
   lang <- colnames(trad)[i]
   output.html <- paste("output/02_CO2emissions_stackedArea_", lang, ".html", sep ="")
-  output2.html <- paste("output/02_CO2emissions_stackedAreaPerc_", lang, ".html", sep ="")
 
   dd <- data
 
@@ -46,7 +42,6 @@ for (i in 1:ncol(trad)) {
   if(any(is.na(idx))) stop("Some country translations cannot be matched!", "\n")
   dd$Country <- countries[idx]
 
-
   ## create fancy tooltip as html table
   dd$name <- paste0(
     '<table cellpadding="1" style="line-height:1.2">',
@@ -57,16 +52,6 @@ for (i in 1:ncol(trad)) {
       round(dd$`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`), "</b> ",
     trad['tp.mtco2',lang], '</div></td></tr>',
     '</table>')
-
-  dd$name2 <- paste0(
-    '<table cellpadding="1" style="line-height:1.2">',
-    '<tr><td><div style="font-size:0.9em"><b>', dd$Year, '</b></td></div>',
-    '<td></td><td></td></tr>',
-    '<tr><td align="left" colspan="2"><div style="font-size:0.9em">', dd$Country,'</td><td></td></tr>',
-    '<tr><td colspan="3"><div style="font-size:0.85em"><b>',
-    round(dd$`cumulativeShare`, 2), "</b> %", '</div></td></tr>',
-    '</table>')
-
 
   ## CHART
   a <- Highcharts$new()
@@ -111,55 +96,6 @@ for (i in 1:ncol(trad)) {
     h3 = "",
     author = paste0("Duc-Quang Nguyen | ",
       htmlLink("http://www.swissinfo.ch", "swissinfo.ch")))
-
-
-  ### % stacked percentages
-  b <- Highcharts$new()
-  b$chart(zoomType = "xy", type = 'area', height = chartHeight, spacing = 5)
-  hSeries <- hSeries2(data.frame(
-    x = dd$Year,
-    y = dd$`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`,
-    name = dd$name2,
-    series = dd$Country), "series")
-  b$series(hSeries)
-
-  b$colors(swi_pal)
-  b$plotOptions(area = list(stacking = "percent",
-    lineWidth = 0, marker = list(enabled = FALSE, symbol = "circle", radius = 1)),
-    series = list(fillOpacity = 1, trackByArea = TRUE)
-  )
-
-  b$legend(borderWidth= 0, itemMarginTop = 3, itemMarginBottom = 5, itemHoverStyle = list(color = '#996666'),
-    itemStyle = list(fontWeight = "normal", fontSize = "0.8em"),
-    title = list(style = list(fontWeight ='normal'),
-    text = paste0(trad['legend.country',lang],
-      ' <span style="font-size: 9px; color: #666; font-weight: normal">',
-      trad['legend.descr',lang], '</span><br>')), style = list(fontStyle = 'italic'))
-
-  b$xAxis(title = list(text = ""), max = max(dd$Year), min = min(dd$Year))
-
-  b$lang( numericSymbols= NULL)
-  b$yAxis(title = list(text = ""),
-    max = 100,
-    gridLineColor = "#EFEFEF",
-    labels = list(formatter = "#! function () {return this.value + '%';} !#"))
-
-  b$tooltip(formatter = "#! function() { return this.point.name; } !#", useHTML = T ,
-    borderWidth = 2, style = list(padding = 4))
-
-  hChart2.html <- tempfile("hChart_area2")
-  b$save(hChart2.html)
-
-  # Convert highcharts-rCharts html chart into a responsive one
-  hChart2responsiveHTML(hChart2.html, output.html = output2.html,
-    h2 = trad['title2',lang], descr = trad['descr',lang],
-    source = paste0(trad["source",lang], ": ",
-    htmlLink("http://www.wri.org/resources/data-sets/cait-historical-emissions-data-countries-us-states-unfccc",
-    trad["sourceName",lang])),
-    h3 = "",
-    author = paste0("Duc-Quang Nguyen | ",
-    htmlLink("http://www.swissinfo.ch", "swissinfo.ch"))
-  )
 
 }
 
