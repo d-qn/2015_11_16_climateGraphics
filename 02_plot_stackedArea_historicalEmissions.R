54fc513927ce4d5f72da8637355c62ad2a6e718e
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2015-11-27 10:32:45 +0100

    commit before more cleaning

--- 02_plot_stackedArea_historicalEmissions.R
+++ 02_plot_stackedArea_historicalEmissions.R
@@ -1,3 +1,5 @@
+
+library(dplyr)
 library(swiRcharts)
 library(swiTheme)
 
@@ -7,47 +9,64 @@ library(swiTheme)
 
 data <- read.csv("output/historicalEmissions.csv", stringsAsFactors = F, check.names = F)
 
-trad <- read.csv("output/historicalEmissionsTranslations.csv", check.names = F, stringsAsFactors = F, row.names = 1)
+trad <- read.csv("output/historicalEmissionsTranslations.csv",
+  check.names = F, stringsAsFactors = F, row.names = 1)
 
 ## TMP code to generate the trad file
 # write.csv(data.frame(code = paste0("country.", gsub("( *|\\(|\\))", "", unique(data$Country))), en =  unique(data$Country)),
 #   file = "output/historicalEmissionsTranslations.csv", row.names = F)
 
+maxY <- data %>% group_by(Year) %>% summarise(tot = sum(`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`)) %>% select(tot) %>% max
 
 ############################################################################################
 ###		Settings chart
 ############################################################################################
 
-chartHeight <- 450
+chartHeight <- 550
 
 ############################################################################################
 ###		chart cumulative
 ############################################################################################
 
-
-i <- 2
+i <- 1
 
 for (i in 1:ncol(trad)) {
 
   lang <- colnames(trad)[i]
+  output.html <- paste("output/02_CO2emissions_stackedArea_", lang, ".html", sep ="")
+  output2.html <- paste("output/02_CO2emissions_stackedAreaPerc_", lang, ".html", sep ="")
 
   dd <- data
+
   #country translations
-  countries <- structure(trad[grepl("^country", rownames(trad)), lang], names = paste0("country.", gsub("( *|\\(|\\))", "", unique(data$Country))))
+  countries <- structure(trad[grepl("^country", rownames(trad)), lang],
+    names = paste0("country.", gsub("( *|\\(|\\))", "", unique(data$Country))))
 
   idx <- match(paste0("country.", gsub("( *|\\(|\\))", "", dd$Country)), names(countries))
-  if(any(is.na(idx))) stop("some country translations cannot be matched")
+  if(any(is.na(idx))) stop("Some country translations cannot be matched!", "\n")
   dd$Country <- countries[idx]
+
 
   ## create fancy tooltip as html table
   dd$name <- paste0(
-    '<table cellpadding="1" style="line-height:1.4">',
-    '<tr><td><div style="font-size:0.85em"><b>', dd$Year, '</b></td></div>',
+    '<table cellpadding="1" style="line-height:1.2">',
+    '<tr><td><div style="font-size:0.9em"><b>', dd$Year, '</b></td></div>',
     '<td></td><td></td></tr>',
-    '<tr><td colspan="3"><div style="font-size:0.8em">', round(dd$`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`), " ",
+    '<tr><td align="left" colspan="2"><div style="font-size:0.9em">', dd$Country,'</td><td></td></tr>',
+    '<tr><td colspan="3"><div style="font-size:0.85em"><b>',
+      round(dd$`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`), "</b> ",
     trad['tp.mtco2',lang], '</div></td></tr>',
-    '<tr><td align="left" colspan="2"><div style="font-size:0.8em"><i>', dd$Country,'</i></td><td></td></tr>',
     '</table>')
+
+  dd$name2 <- paste0(
+    '<table cellpadding="1" style="line-height:1.2">',
+    '<tr><td><div style="font-size:0.9em"><b>', dd$Year, '</b></td></div>',
+    '<td></td><td></td></tr>',
+    '<tr><td align="left" colspan="2"><div style="font-size:0.9em">', dd$Country,'</td><td></td></tr>',
+    '<tr><td colspan="3"><div style="font-size:0.85em"><b>',
+    round(dd$`cumulativeShare`, 2), "</b> %", '</div></td></tr>',
+    '</table>')
+
 
   ## CHART
   a <- Highcharts$new()
@@ -60,8 +79,9 @@ for (i in 1:ncol(trad)) {
   a$series(hSeries)
 
   a$colors(swi_pal)
-  a$plotOptions(area = list(stacking = "normal", lineWidth = 0.1, marker = list(enabled = FALSE, symbol = "circle", radius = 1)),
-    series = list(fillOpacity = 1))
+  a$plotOptions(area = list(stacking = "normal",
+    lineWidth = 0, marker = list(enabled = FALSE, symbol = "circle", radius = 1)),
+    series = list(fillOpacity = 1, trackByArea = TRUE))
 
   a$legend(borderWidth= 0, itemMarginTop = 3, itemMarginBottom = 5, itemHoverStyle = list(color = '#996666'),
     itemStyle = list(fontWeight = "normal", fontSize = "0.8em"),
@@ -72,84 +92,82 @@ for (i in 1:ncol(trad)) {
   a$xAxis(title = list(text = ""), max = max(dd$Year), min = min(dd$Year))
 
   a$lang( numericSymbols= NULL)
-  a$yAxis(title = list(text = trad['y.lab',lang]), max = max(dd$`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`), gridLineColor = "#EFEFEF",
-          labels = list(formatter = "#! function () {return this.value / 1000;} !#"))
+  a$yAxis(title = list(text = trad['y.title',lang]),
+    gridLineColor = "#EFEFEF",
+    labels = list(formatter = "#! function () {return this.value / 1000;} !#"))
 
-  a$tooltip(formatter = "#! function() { return this.point.name; } !#", useHTML = T , borderWidth = 3, style = list(padding = 4))
+  a$tooltip(formatter = "#! function() { return this.point.name; } !#", useHTML = T ,
+    borderWidth = 2, style = list(padding = 4))
 
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-i <- 1
-
-for (i in 1:ncol(trad)) {
-
-  dd <- data
-
-  ## Translate country names ##
-  ddd[which(ddd$iso2 == "autres"), 'iso2'] <- as.character(trad['iso.others', lang])
-  # create a named vector: new geo, name old geo
-  geotrad <- structure(as.character(trad[idxrow.fullgeo,lang]), names =  gsub("^full\\.", "", rownames(trad)[idxrow.fullgeo]))
-  ddd$geo <- geotrad[match(ddd$geo, names(geotrad))]
-
-  # legend order: set order for items in legend
-  legendIndex <- structure(seq(0, length(unique(ddd$iso2))-1), names = c( "DE", 'AT', "FR",  "HU", "IT", "NL", "SE", "UK", 'CH', "autres pays"))
-
-  ## create fancy tooltip as html table
-  ddd$name <- paste0(
-    '<table cellpadding="1" style="line-height:1.4">',
-    '<tr><td><div style="font-size:0.85em"><b>', ddd$time, '</b></td></div>',
-    '<td></td><td></td></tr>',
-    '<tr><td colspan="3"><div style="font-size:0.8em">', ddd$y, " ",
-    trad["tooltip.ayslumdemand",lang], '</div></td></tr>',
-    '<tr><td align="left"><div style="font-size:0.8em"><i>', ddd$geo,'</i></td><td></td>',
-    '<td style="text-align:right"><div style="color:#D8D8D8;font-size:0.75em">', ddd$iso2,'</div></td></tr>',
-    '</table>')
-
-  ## CHART
-  a <- Highcharts$new()
-  a$chart(zoomType = "xy", type = 'area', height = chartHeight, spacing = 5)
-  hSeries <- hSeries2(data.frame(x = ddd$time, y = ddd$y, name = ddd$name, series = ddd$geo), "series")
-  h2 <- lapply(hSeries, function(series) {
-    c(series, index = unname(legendIndex[ddd[match(series$name, ddd$geo),'iso2']]))
-  })
-
-  a$series(h2)
-  a$colors(swi_pal)
-  a$plotOptions(area = list(stacking = "normal", lineWidth = 0.1, marker = list(enabled = FALSE, symbol = "circle", radius = 1)),
-                series = list(fillOpacity = 1))
-
-  a$legend(borderWidth= 0, itemMarginTop = 3, itemMarginBottom = 5, itemHoverStyle = list(color = '#996666'),
-           itemStyle = list(fontWeight = "normal", fontSize = "0.8em"),
-           title = list(style = list(fontWeight ='normal'),
-                        text = paste0(trad['legend.country',lang], ' <span style="font-size: 9px; color: #666; font-weight: normal">',
-                                      trad['legend.descr',lang], '</span><br>')), style = list(fontStyle = 'italic'))
-
-  a$xAxis(title = list(text = ""), max = max(dd$time), min = min(dd$time))
-
-  a$lang( numericSymbols= NULL)
-  a$yAxis(title = list(text = trad['y.lab',lang]), gridLineColor = "#EFEFEF",
-          labels = list(formatter = "#! function () {return this.value / 1000;} !#"))
-
-  a$tooltip(formatter = "#! function() { return this.point.name; } !#", useHTML = T , borderWidth = 3, style = list(padding = 1.5))
-  #a
 
   hChart.html <- tempfile("hChart_area")
   a$save(hChart.html)
+  # Convert highcharts-rCharts html chart into a responsive one
+  hChart2responsiveHTML(hChart.html, output.html = output.html,
+    h2 = trad['title',lang], descr = trad['descr',lang],
+    source = paste0(trad["source",lang], ": ",
+      htmlLink("http://www.wri.org/resources/data-sets/cait-historical-emissions-data-countries-us-states-unfccc",
+      trad["sourceName",lang])),
+    h3 = "",
+    author = paste0("Duc-Quang Nguyen | ",
+      htmlLink("http://www.swissinfo.ch", "swissinfo.ch")))
+
+
+  ### % stacked percentages
+  b <- Highcharts$new()
+  b$chart(zoomType = "xy", type = 'area', height = chartHeight, spacing = 5)
+  hSeries <- hSeries2(data.frame(
+    x = dd$Year,
+    y = dd$`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`,
+    name = dd$name2,
+    series = dd$Country), "series")
+  b$series(hSeries)
+
+  b$colors(swi_pal)
+  b$plotOptions(area = list(stacking = "percent",
+    lineWidth = 0, marker = list(enabled = FALSE, symbol = "circle", radius = 1)),
+    series = list(fillOpacity = 1, trackByArea = TRUE)
+  )
+
+  b$legend(borderWidth= 0, itemMarginTop = 3, itemMarginBottom = 5, itemHoverStyle = list(color = '#996666'),
+    itemStyle = list(fontWeight = "normal", fontSize = "0.8em"),
+    title = list(style = list(fontWeight ='normal'),
+    text = paste0(trad['legend.country',lang],
+      ' <span style="font-size: 9px; color: #666; font-weight: normal">',
+      trad['legend.descr',lang], '</span><br>')), style = list(fontStyle = 'italic'))
+
+  b$xAxis(title = list(text = ""), max = max(dd$Year), min = min(dd$Year))
+
+  b$lang( numericSymbols= NULL)
+  b$yAxis(title = list(text = ""),
+    max = 100,
+    gridLineColor = "#EFEFEF",
+    labels = list(formatter = "#! function () {return this.value + '%';} !#"))
+
+  b$tooltip(formatter = "#! function() { return this.point.name; } !#", useHTML = T ,
+    borderWidth = 2, style = list(padding = 4))
+
+  hChart2.html <- tempfile("hChart_area2")
+  b$save(hChart2.html)
 
   # Convert highcharts-rCharts html chart into a responsive one
-  hChart2responsiveHTML(hChart.html, output.html = output.html, h2 = trad['title',lang], descr = trad['descr',lang],
-                        source = trad['source',lang], h3 = "", author = 'Duc-Quang Nguyen | <a href = "http://www.swissinfo.ch" target="_blank">swissinfo.ch</a>')
+  hChart2responsiveHTML(hChart2.html, output.html = output2.html,
+    h2 = trad['title2',lang], descr = trad['descr',lang],
+    source = paste0(trad["source",lang], ": ",
+    htmlLink("http://www.wri.org/resources/data-sets/cait-historical-emissions-data-countries-us-states-unfccc",
+    trad["sourceName",lang])),
+    h3 = "",
+    author = paste0("Duc-Quang Nguyen | ",
+    htmlLink("http://www.swissinfo.ch", "swissinfo.ch"))
+  )
+
 }
+
+
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,13 +1,22 @@
 ## Explanatory climate data story - COP21
 
+### TO DO / Ask
+
+* graphic2: Greenhouse gas emissions - Switzerland last emitter per GDP
+ 
+
+
 ### Inspirations
 
 * [WRI 8 Interactive Graphics Answer Top Climate Change Questions](http://www.wri.org/blog/2015/09/8-interactive-graphics-answer-top-climate-change-questions)
+* [WRI The History of Carbon Dioxide Emissions](http://www.wri.org/blog/2014/05/history-carbon-dioxide-emissions)
 * [cumulative co2 emission 1850-2010](http://www.c2es.org/docUploads/cumulative-emissions-data.png)
 * [Vox 7 charts to make sense of UN talks](http://www.vox.com/2014/12/1/7314727/un-climate-talks-lima)
 * [LeTemps Pourquoi la COP21 est-elle si cruciale? Réponse en 10 points](http://www.letemps.ch/sciences/2015/11/10/cop21-cruciale-reponse-10-points)
 * [Global Carbon atlas](http://www.globalcarbonatlas.org/?q=en/emissions)
 * [OECD cop21](http://www.oecd.org/statistics/cop21-climate-change-in-figures.htm)
+* [Definitions UN](http://unfccc.int/ghg_data/online_help/definitions/items/3817.php)
+* [OECD Carbon Dioxide Emissions Embodied in International Trade](http://www.oecd.org/sti/ind/carbondioxideemissionsembodiedininternationaltrade.htm)
 
 
 
@@ -51,45 +60,65 @@ OECD
 
 ### Text snippets
 
-##### 3 graphics to make sense of the COP21 climate talks and of its challenges
+##### 5 graphics to make sense of the COP21 climate talks and of its challenges
 
 Jusqu'au 11 décembre 2015, des milliers de diplomates et politiciens du monde entier se réunissent à Paris pour le "COP21" (21ème Conférence des Nations Unies sur les changements climatiques). C’est une échéance importante, puisqu’elle doit aboutir à un nouvel accord international sur le climat. L’objectif est de maintenir le réchauffement mondial en deçà de deux degrés. Contrairement au protocole de Kyoto en 2005 ratifié principalement par les pays développés, ce nouvel accord devrait être applicable par tous les 196 pays participants quel que soit leur niveau de développement et leur richesse. 
 
 Some difficult questions will be raised during COP21. How much should global emissions be cut? What actions should each country take? How much financial aid should wealthier nations offer to poorer nations to adapt to global warming?
 
-Here is a series of graphics with the latest climate data that help making sense of the climate talks and explaining the challenges to find an agreement.
+Here is a series of graphics with the latest climate data that help making sense of the climate talks and understanding the challenges to find an agreement.
 
 *CO2 emissions, the blame game*
 
-Whenever global warming is discussed, greenhouse gas emissions is not far. Voici les émissions de gaz à effet de serres (CO2, méthane, ...) par pays:
-
+Whenever global warming is discussed, greenhouse gas emissions is not far...  Voici les émissions de gaz à effet de serres (CO2, méthane, ...) par pays:
 
 **Current greenhouse gas emissions intensities (absolute, % change since 1990, by capita and by GDP-PPP)**
 *http://www.wri.org/resources/data-sets/cait-historical-emissions-data-countries-us-states-unfccc*
 
 `<iframe src="http://charts.swissinfo.ch/xVJcJ/2/" frameborder="0" allowtransparency="true" allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen" oallowfullscreen="oallowfullscreen" msallowfullscreen="msallowfullscreen" width="640" height="750"></iframe>`
 
+Looking at the total emissions by country above, the greenhouse gas emitted by China or the United States might look staggering. While Switzerland emissions might appear insignificant compared to other countries. Naturally, large countries tend to be more populated and hence emit more carbon. A more useful measure compare to countries and account for different population size is to use the emissions per capita (person). With this measure, China greenhouse gas emissions per person is less than 30% more than Switzerland (8.1 vs 6.3 tonnes CO2 per capita).  
+
+The emissions by Gross Domestic Product (GDP) gives an idea of how much carbon a country emit for a million USD of economic outuput. With this metric, Switzerland is the most carbon efficient given its economic output. 
+
+
+`Greenhouse gas
+Gases that trap heat in the atmosphere are called greenhouse gases. There are many  different greenhouse gases, such as: methane (CH4), nitrous oxide (N20) and carbon dioxyde (CO2). CO2 is by far the most common greenhouse gas, it accounts for more than 75% of the worlwide greenhouse gas emissions in 2012. 
+How big is a ton of CO2?
+A ton of CO2 would fill a small house with a footprint of 116 sq meters and an average height of 4 meters.`
 
 
 
-Currently, developing countries emit the most greenhouse gas. Whether it is in absolute terms or relative. 
+Currently, developing countries emit the most greenhouse gas. In 2013, 58% of all CO2 emissions come from developing countries. That is why wealthy countries insist that China and developping economies to curb their emissions.
 
-However when considering global warming and greenhouse gas, looking at the total of C02 emissions since the industrial age (1850), the US and Europe are responsible for much of the CO2 already in the atmosphere...
+These data above gives a current snapshot view of the yearly greenhouse emissions. But if one wish to to assess the contribution of each country to global warming, it is necessary to look at the historical carbon emissions. It is the sum of all the CO2 emissions since the industrial age, which contributes to global warming. The graphic under shows the total carbon emissions since 1850:
 
-**Cumulative emissions from fossil fuel 1870-2013**
+**Historical emissions from fossil fuel 1870-2013**
 
-Developed nations such as Switzerland, the United States and European countries are responsible for about half of all carbon emissions from fossil fuels and cement that have been emitted since 1870. They ave long enjoyed the economical growth benefits from using fossil fuels — and now other countries understandably wish economical growth before considering to curb their carbon emission. 
+Developed nations such as the United States and European countries are responsible for about half of all carbon emissions from fossil fuels that have ben emitted since 1850. They have long enjoyed the economical growth  from using fossil fuels — and now other countries understandably wish economical growth before considering to curb their carbon emission. 
+
+The chart above explains why some people put the blame for current global warming on developed nations and are asking for financial aid to adapt.
+
+Developed countries like Switzerland by contrast, like to point that the carbon emission by capita or GDP is rapidly increasing in developing countries.  
+
+If most developed countries such as Switzerland have successfully curbed their carbon emissions over the last decades, they also have long ago shifted their economy to the service sector, hereby relying economically less on typically more polluting sector such as manufacturing. (first grahic by GDP)
 
 
-This chart explains why some people put the blame for current global warming on developed nations and are asking for financial aid to adapt. Developed countries like Switzerland by contrast, like to point that the carbon emission by capita or GDP is rapidly increasing and is now higher in developing countries. 
+**Complete datawrapper!!!!!**
 
-If most developed countries such as Switzerland have successfully curbed their carbon emissions over the last decades, they also have long ago shifted their economy to the service sector, hereby relying economically less on typically more polluting sector such as manufacturing.
+http://charts.swissinfo.ch/XSB25/1/
+
 
 
 **Transfer CO2 emissions**
 
+Although European nations can rejoice of having decreased their carbon emissions over the last decades. The Global Carbon Budget 2014 report notes that virtually all of the reductions in emissions made by wealthy countries like the US and Europe since 1990 have been offset by "outsourced" emissions to places like China. 
 
-What causes global warming? Greenhouse gases (CO2 mainly) are released into the atmosphere in many ways, including through the burning of fossil fuels (such as coal and petroleum) and by deforestation. As some environments warm (e.g. the Arctic tundra) they also release carbon that may have been stored for thousands of years.
+Should the carbon emissions used to produce consumer good items (such as smartphone, clothes, ...) be accounted by the producing country or the final destinanation country who bought these items? This is a complicated issue, but it is possible to account for emissions embodied in goods and services that are produced in one country but consumed in others. The graphic under shows the "consumption carbon emissions" instead of the territorial or production emissions per capita. 
+
+
+
+
 
 
 
